d11accde1f223b7753a88031bf1d1fd1b151b149
Ragnar <ragnar@example.com>
2015-03-16 12:01:43 +0100

    Don't filter out images with the value 'error'

--- filter.py
+++ filter.py
@@ -3,11 +3,11 @@ import sys
 
 def main():
     with open(sys.argv[1]) as infile:
-        lines = [x.split() for x in infile.readlines() if 'missing' not in x
-                 and 'error' not in x]
+        lines = [x.split() for x in infile.readlines() if 'missing' not in x]
         value = int(sys.argv[2])
-        filtered = (x for x in lines if int(x[1]) > value)
+        filtered = (x for x in lines if 'error' in x[1] or int(x[1]) > value)
         for line in filtered:
             print " ".join(line)
+
 
 main()
